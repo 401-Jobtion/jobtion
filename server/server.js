e355870260e765fbbba982a81b7ee2e2fad10c1c
Chirayu Shah <chirayushah61@gmail.com>
2026-01-19 14:53:26 -0700

    mongo removal

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 
 import extractRouter from './routes/extract/route.js';
@@ -25,11 +24,6 @@ app.use('/api/tailor-resume', tailorResumeRouter);
 app.get('/', (req, res) => {
     res.send('Jobtion API is running...');
 });
-
-// Database Connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error('MongoDB Connection Error:', err));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
